5cf942b756169f840dc15e4a681e90cefded933b
Rafael Rocha de Azevedo <rrochadeazevedo@gmail.com>
2021-05-08 12:20:46 -0300

    feat: seting the correct project infos  at config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Mais Fácil Contabilidade`,
+    description: `Landing page para o sistema de contabilidade Mais Fácil`,
+    author: `Rafael Rocha de Azevedo`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
