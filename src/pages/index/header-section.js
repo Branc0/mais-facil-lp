9bc0faa7d182f9ed51894dedc5bf2f20ddcc87f5
Rafael Rocha de Azevedo <rrochadeazevedo@gmail.com>
2021-05-08 12:07:49 -0300

    feat: creates the initial header for index

---
+++ src/pages/index/header-section.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+import styled from "styled-components"
+// import { graphql, useStaticQuery } from "gatsby"
+
+const Section = styled.section`
+    min-height: 45.8rem;
+    background-color: #000;
+`
+const HeaderSection = () => {
+    return (
+        <Section>
+
+        </Section>
+    )
+}
+
+export default HeaderSection;
