748ec356b8f1f158f42a5af412709c7461707c28
Rafael Rocha de Azevedo <rrochadeazevedo@gmail.com>
2021-05-15 18:28:17 -0300

    feat: add button for the login page on header

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -2,10 +2,11 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import styled from "styled-components";
 import { Navbar, Nav } from "react-bootstrap"
+
 import LogoSvg from "../../images/white-logo.svg"
 
 
-const HeaderElement = styled.header` 
+const HeaderElement = styled.header`
   background-color: #206A5D;
   width:100%;
   position: fixed;
@@ -15,6 +16,28 @@ const HeaderElement = styled.header`
 const CustomH1 = styled.h1`
   margin-bottom: 0;
   line-height: 1;
+`
+
+const CustomA = styled.a`
+  border-radius: 25px;
+  color: #FAFAFA;
+  background: #81B315;
+  border: none;
+  height: 24px;
+  line-height: 24px;
+  padding: 0 24px;
+  text-transform: uppercase;
+  font-size: 14px;
+  font-family: roboto;
+  font-weight: 700;
+  letter-spacing: 1.25px;
+  margin-top: 8px;
+  text-decoration: none;
+  margin-left: 12px;
+  &:hover{
+  color: #206A5D;
+    cursor: pointer;
+  }
 `
 
 const Header = ({ siteTitle }) => (
@@ -27,12 +50,13 @@ const Header = ({ siteTitle }) => (
           </CustomH1>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse className='justify-content-end' id="basic-navbar-nav">
+        <Navbar.Collapse className='justify-content-end align-items-center' id="basic-navbar-nav">
           <Nav className='text-end'>
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Como Funciona</Nav.Link>
             <Nav.Link href="#link">Planos</Nav.Link>
             <Nav.Link href="#link">Contato</Nav.Link>
+            <CustomA>Entrar</CustomA>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
